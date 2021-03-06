353c609afe6d24f1f17b56d797b8390971087fab
adnan1990877@gmail.com <Md Adnan Khan>
2021-03-06 17:16:58 +0530

    Added user model, password encryption, user login, authorization protection and role authorization

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
@@ -16,11 +17,15 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
 // Body Parser
 app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -36,6 +41,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Mount the routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
