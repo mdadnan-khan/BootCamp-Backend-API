25ce93ef54185d7c1184e36d977cc9d09ce06e76
adnan1990877@gmail.com <Md Adnan Khan>
2021-03-07 02:49:01 +0530

    Added review model and associated CRUD functionality

--- models/Review.js
+++ models/Review.js
@@ -25,13 +25,44 @@ const ReviewSchema = new mongoose.Schema({
     bootcamp: {
         type: mongoose.Schema.ObjectId,
         ref: "Bootcamp",
-        required: true,
     },
     user: {
         type: mongoose.Schema.ObjectId,
         ref: "User",
-        required: true,
     },
 });
+
+// Statics - method to get average rating
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId },
+        },
+        {
+            $group: { _id: "$bootcamp", averageRating: { $avg: "$rating" } },
+        },
+    ]);
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Call getAverageRating after save
+ReviewSchema.post("save", function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre("remove", function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Prevent user from submitting more than one review per bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
 module.exports = mongoose.model("Review", ReviewSchema);
